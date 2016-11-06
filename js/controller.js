4d26d7f8d868cf160d21f44ef72731a00dd8571e
stevenpan78 <karimoman7@gmail.com>
2016-11-05 23:57:34 -0400

    checkboxes are done

---
+++ js/controller.js
@@ -0,0 +1,8 @@
+angular
+	.module("angApp",[])
+	.controller("myController",function($scope,$rootScope){
+		$scope.truth1 = true;
+		$scope.truth2 = false;
+		$scope.truth3 = true;
+		$scope.truth4 = false;
+	});
